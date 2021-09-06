efe574e232768ee2f042bf23ce626b847752ad5e
Alexander Liman <liman324@yandex.ru>
2021-09-06 10:06:44 +0600

    Create test4.ino

---
+++ test4.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <4x14LED.h>
+  HT4x14LED led;
+ 
+
+void setup() {
+   Wire.begin();
+   Serial.begin(9600);
+   led.init(1);
+   led.brig(10);
+   led.blink(1);
+   led.clear();
+}
+
+void loop() {
+ led.print(247, 3,   0x1201, -1,-1,-1);
+ delay(1000);
+}
